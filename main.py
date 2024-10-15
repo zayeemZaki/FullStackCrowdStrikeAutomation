a9af8a0de066687b47313af88a3388df647c3d36
Zayeem Zaki <137522702+zayeemZaki@users.noreply.github.com>
2024-10-15 08:14:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,11 @@ app = create_app()
 
 @app.before_request
 def before_request():
-<<<<<<< HEAD
-    ip_address = '10.1.80.199'
-=======
     ip_address = '#.#.#.#'
->>>>>>> 64118003c07b97454f4f5e4f1de52452b9adb000
     g.ip_address = ip_address
     g.port = '####'
 
 if __name__ == '__main__':
-<<<<<<< HEAD
-    ip_address = '10.1.80.199'
-    port = 8000
-=======
     ip_address = '#.#.#.#'
     port = ####
->>>>>>> 64118003c07b97454f4f5e4f1de52452b9adb000
     app.run(host=ip_address, port=port, debug=True)
