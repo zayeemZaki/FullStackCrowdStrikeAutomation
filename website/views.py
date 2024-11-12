f165442fdd75069413f2eb6841de4b61476c7d0b
Rajiv Kaushik <zayeemzaki45@gmail.com>
2024-11-12 09:03:20 -0500

    updated auth.html

--- website/views.py
+++ website/views.py
@@ -1,9 +1,5 @@
-from flask import Blueprint, render_template, request, flash, session, redirect, url_for, g
+from flask import Blueprint, render_template, g
 from flask_login import login_required, current_user
-import requests
-import pandas as pd
-from datetime import datetime, timezone
-from falconpy import RealTimeResponse
 
 views = Blueprint('views', __name__)
 
